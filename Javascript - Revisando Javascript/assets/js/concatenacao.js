75c0f7a5ae843075989223fd96e36066f142f714
Cicera Ribeiro <maccicera@gmail.com>
2022-06-17 16:42:33 -0300

    ok

---
+++ Javascript - Revisando Javascript/assets/js/concatenacao.js
@@ -0,0 +1,32 @@
+
+var nome = 'Cicera';
+var idade = 35;
+
+var nome1 = prompt('Digite o seu nome:');
+var idade1 = prompt('Digite sua idade:');
+var idade2 = 50;
+
+
+console.log(idade1);
+console.log(idade2);
+
+document.write('<h1>Olá ' + nome1 + ', tudo bem? Estou vendo aqui que você possui '+ idade1 +', anos </h1>');
+
+//Valores Null e Undefined
+//Null
+var test = null
+console.log(test)
+
+
+test = 'Valor modificado com sucesso'
+console.log(test)
+
+
+test = 127.48
+console.log(test)
+
+//Andefined
+var teste2 //undefined - sessão indefinida
+console.log(teste2)
+
+
