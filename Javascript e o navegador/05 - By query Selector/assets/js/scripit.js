cff82bd593a6a33da88670d6c71dc4692f5ed26a
Cicera Ribeiro <maccicera@gmail.com>
2022-06-18 11:08:11 -0300

    treinamento

---
+++ Javascript e o navegador/05 - By query Selector/assets/js/scripit.js
@@ -0,0 +1,9 @@
+console.log(document.querySelector('#container h1'));
+
+console.log(document.querySelector('h1'));
+
+console.log(document.querySelector('#segundo'));
+
+console.log(document.querySelector('footer h2'));
+
+console.log(document.querySelector('ul .itens-azuis'));
